161ca8b15321de05b2bafa386e4a730587f55707
Kostiantyn <krokotkoleshnia@gmail.com>
2022-11-19 22:49:20 +0200

    Main commit

---
+++ 11.10.22_Lab_7/11.10.22_Lab_7.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N = 0, divider = 0, multiply = 1, temp;
+    int i = 1, j = 0;
+    int last = 0;
+    cout << "Введіть число:\n";
+    cin >> N;
+    int n = N;
+   
+    while (N > 10){
+        N /= pow(10, i);
+        N *= pow(10, i);
+        if (N == 0)
+        {
+            break;
+        }
+        else if (int((n - N) / pow(10, j)) % 2 == 0)
+        {
+            multiply *= int((n - N) / pow(10, j));
+        }
+        i++; j++;
+    } 
+
+    if (int(n / pow(10, j)) % 2 == 0)
+    {
+        multiply *= int(n / pow(10, j));
+    }
+    else if (multiply == 1)
+    {
+        multiply = 0;
+    }
+    cout << "multiply = " << multiply << endl;
+}
---
+++ Task_5/Task_5.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+int digitSum(int n)
+{
+    if (n < 10) return n;
+    int res = n / 10;
+    return n - res * 10 + digitSum(res);
+}
+int main() {
+    int a;
+    int res = 0;
+    int temp;
+    cin >> a;
+    while (true)
+    {
+        if (a < 10) {
+            res += a;
+            break;
+        }
+        else
+        {
+            temp = a / 10;
+            res += a - temp * 10;
+        }
+        
+    }
+    cout << res;
+}
